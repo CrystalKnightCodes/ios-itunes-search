1a293547ad084c2874d807c23e200a08e7ad7cea
CrystalKnightCodes <CrystalKnightCodes@gmail.com>
2019-11-05 18:18:31 -0500

    Initial configuration of Result Type file

---
+++ iTunes Search/iTunes Search/ResultType.swift
@@ -0,0 +1,14 @@
+//
+//  ResultType.swift
+//  iTunes Search
+//
+//  Created by Christy Hicks on 11/5/19.
+//  Copyright © 2019 Knight Night. All rights reserved.
+//
+
+import Foundation
+enum ResultType: String {
+    case software
+    case musicTrack
+    case movie
+}
